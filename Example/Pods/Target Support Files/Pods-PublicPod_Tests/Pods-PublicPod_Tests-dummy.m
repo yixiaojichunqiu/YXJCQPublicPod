95f9b3e4ebbddb0b0ae8dee292dba6412ce204ff
yixiaojichunqiu <fdk785126868@qq.com>
2019-03-27 14:41:06 +0800

    1

---
+++ Example/Pods/Target Support Files/Pods-PublicPod_Tests/Pods-PublicPod_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PublicPod_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_PublicPod_Tests
+@end
---
+++ Example/Pods/Target Support Files/PublicPod/PublicPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PublicPod : NSObject
+@end
+@implementation PodsDummy_PublicPod
+@end
